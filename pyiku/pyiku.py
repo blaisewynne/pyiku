4454fc2cccd4884cad9ed579d2c8ef84ff3facb5
bmw-pc <blaise.wynne1@gmail.com>
2026-02-27 16:26:23 +1100

    version 0.3

--- pyiku/pyiku.py
+++ pyiku/pyiku.py
@@ -3,36 +3,39 @@ class Pyiku:
         word_length = len(word) - 1
         vowels = ['a', 'e', 'i', 'o', 'u']
         consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-
         count = 0
         word = word.lower()
 
         for index, letter in enumerate(word):
-            if letter in vowels:
-                if word[index+1] in vowels:
-                    count += 1
-                elif word[index-1] in consonants:
-                    count += 1
 
+            if letter in vowels:
+                try:
+                    if index != word_length:
+                        if word[index+1] in vowels:
+                            count += 1
+                        elif word[index-1] in consonants:
+                            count += 1
+                except IndexError:
+                    continue 
             if index == word_length and letter == 'e':
                 break
-        
         if count == 0:
             count += 1
+
+        return count
 
     def is_haiku(haiku: str):
         haiku = haiku.lower()
         total_syllables = []
         for lines in haiku.split(','):
-            for sentence in lines.split(' '):
-                syllables = Pyiku.count_syllables(sentence)
-                total_syllables.append(syllables)
+            for words in lines.split(' '):
+                if words != '':
+                    syllables = Pyiku.count_syllables(words)
+                    total_syllables.append(syllables)
+
         if sum(total_syllables) == 17:
             return True
-        else:
-            return False
-            
 
-Pyiku.count_syllables('supercalifragilisticexpialidocious')
-Pyiku.count_syllables('coin')
-Pyiku.count_syllables('button')
+
+
+
--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,12 @@
 from pyiku import Pyiku
+
+# 14
+Pyiku.count_syllables('supercalifragilisticexpialidocious')
+# 4
+Pyiku.count_syllables('aspiration')
+# 3
+Pyiku.count_syllables('absolute')
+# 2
+Pyiku.count_syllables('button')
+# 1
+Pyiku.count_syllables('pig')
